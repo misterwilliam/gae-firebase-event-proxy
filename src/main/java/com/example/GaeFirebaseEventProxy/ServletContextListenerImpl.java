f3f2992fc5fbd293545d55bcaef7380eb37177f5
misterwilliam <mr.williamchang@gmail.com>
2016-04-26 16:21:07 -0700

    Created a simple ServletContextListener that just outputs a status message

---
+++ src/main/java/com/example/GaeFirebaseEventProxy/ServletContextListenerImpl.java
@@ -0,0 +1,21 @@
+package com.example.GaeFirebaseEventProxy;
+
+import java.util.logging.Logger;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class ServletContextListenerImpl implements ServletContextListener {
+
+  private static final Logger log = Logger.getLogger(ServletContextListener.class.getName());
+
+  @Override
+  public void contextInitialized(ServletContextEvent event) {
+    log.info("Starting ....");
+  }
+
+  @Override
+  public void contextDestroyed(ServletContextEvent event) {
+    // App Engine does not currently invoke this method.
+  }
+}
