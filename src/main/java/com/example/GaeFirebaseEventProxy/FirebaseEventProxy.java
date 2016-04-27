3665577ba0d02e8cd4fc06febfbe2dcb7c0fc5ed
misterwilliam <mr.williamchang@gmail.com>
2016-04-27 12:59:12 -0700

    Forward events from Firebase

--- src/main/java/com/example/GaeFirebaseEventProxy/FirebaseEventProxy.java
+++ src/main/java/com/example/GaeFirebaseEventProxy/FirebaseEventProxy.java
@@ -3,24 +3,90 @@ package com.example.GaeFirebaseEventProxy;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 import java.util.logging.Logger;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.firebase.client.AuthData;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 import com.firebase.security.token.TokenGenerator;
+import com.google.appengine.api.utils.SystemProperty;
 
 public class FirebaseEventProxy {
 
   private static final Logger log = Logger.getLogger(FirebaseEventProxy.class.getName());
 
+  private String firebaseAuthToken;
+
   public FirebaseEventProxy() {
-    String firebaseSecret = this.getFirebaseSecret();
-    log.info("Token: " + this.getFirebaseAuthToken(firebaseSecret));
+    this.firebaseAuthToken = this.getFirebaseAuthToken(this.getFirebaseSecret());
   }
 
   public void start() {
+    String FIREBASE_LOCATION = "https://gae-fb-proxy.firebaseio.com/";
+    Firebase firebase = new Firebase(FIREBASE_LOCATION);
+    // Authenticate with Firebase
+    firebase.authWithCustomToken(this.firebaseAuthToken, new Firebase.AuthResultHandler() {
+      @Override
+      public void onAuthenticationError(FirebaseError error) {
+        log.severe("Firebase login error: " + error.getMessage());
+      }
 
+      @Override
+      public void onAuthenticated(AuthData auth) {
+        log.info("Firebase login successful");
+      }
+    });
+
+    // Subscribe to value events. Depending on use case, you may want to subscribe to child events
+    // through childEventListener.
+    firebase.addValueEventListener(new ValueEventListener() {
+      @Override
+      public void onDataChange(DataSnapshot snapshot) {
+        if (snapshot.exists()) {
+          try {
+            // Convert value to JSON using Jackson
+            String json = new ObjectMapper().writeValueAsString(snapshot.getValue());
+            URL dest = new URL("http://localhost:9000/api/log");
+            HttpURLConnection connection = (HttpURLConnection) dest.openConnection();
+            connection.setDoOutput(true);
+            // Rely on X-Appengine-Inbound-Appid to authenticate. Turning off redirects is
+            // required to enable.
+            connection.setInstanceFollowRedirects(false);
+            // Fill out header if in dev environment
+            if (SystemProperty.environment.value() != SystemProperty.Environment.Value.Production) {
+              connection.setRequestProperty("X-Appengine-Inbound-Appid", "dev-instance");
+            }
+            // Put Firebase data into http request
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("&fbSnapshot=");
+            stringBuilder.append(URLEncoder.encode(json, "UTF-8"));
+            connection.getOutputStream().write(stringBuilder.toString().getBytes());
+            if (connection.getResponseCode() != 200) {
+              log.severe("Forwarding failed");
+            }
+          } catch (JsonProcessingException e) {
+            log.severe("Unable to convert Firebase response to JSON: " + e.getMessage());
+          } catch (IOException e) {
+            log.severe("Error in connecting to app engine: " + e.getMessage());
+          }
+        }
+      }
+
+      @Override
+      public void onCancelled(FirebaseError error) {
+        log.severe("Firebase connection cancelled: " + error.getMessage());
+      }
+    });
   }
 
   private String getFirebaseSecret() {
